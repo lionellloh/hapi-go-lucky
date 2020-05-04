fd9265baa1894c9a71114f48fd4c7a9f608de253
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-05-05 06:03:27 +0800

    parameters w routes

---
+++ program.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+var server = Hapi.Server({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+
+server.route({
+    method: 'GET',        // define the method this route will handle
+    path: `/{name*}`, // this is how you capture route parameters in Hapi
+    handler: ((req, h) => {
+        console.log(req.params);
+        return `Hello ${req.params.name}`
+    })
+});
+
+async function startServer() {
+    await server.start() // start the Hapi server on your localhost
+    console.log('Now Visit: http://localhost:' + server.info.port + '/');
+}
+
+startServer();
+
+module.exports = server;
+
+
+
